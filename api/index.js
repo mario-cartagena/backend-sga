9616264f99dea1ee1cabbf14d7c9307163e3542a
mario-cartagena <jhonmariocartagena@gmail.com>
2023-07-05 19:51:32 -0500

    test

--- api/index.js
+++ api/index.js
@@ -1,8 +1,9 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // <== Will be created later
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3001; // <== You can change the port
+
 server.use(middlewares);
 server.use(router);
-server.listen(port);
+
+module.exports = server;
