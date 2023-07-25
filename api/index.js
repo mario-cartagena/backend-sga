f768274c85156795be05054620fba10898e50c39
mario-cartagena <jhonmariocartagena@gmail.com>
2023-07-24 23:22:54 -0500

    configuraciones para despliegue en render

--- api/index.js
+++ api/index.js
@@ -1,19 +1,20 @@
-// See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
 
-server.use(middlewares)
-// Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
-server.use(router)
-server.listen(3000, () => {
-    console.log('JSON Server is running')
+const port = process.env.PORT || 3000;
+
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', '*')
+    res.header('Access-Control-Allow-Methods', '*');
+    next()
 })
 
-// Export the Server API
-module.exports = server
+server.use(router);
+server.use(middlewares)
+
+server.listen(port, () => {
+    console.log('JSON Server is running')
+})
